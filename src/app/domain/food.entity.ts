d4cd14d201112a0e6795b4a7e6be2db271d2d525
DanMarques31 <daniellmarques2003@hotmail.com>
2024-11-20 23:02:47 -0300

    Getting food.entity.ts into Type ORM

--- src/app/domain/food.entity.ts
+++ src/app/domain/food.entity.ts
@@ -1,10 +1,24 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('food')
 export class Food {
     
+    @PrimaryGeneratedColumn({ name: "food_id"} )
     id: number;
+
+    @Column()
     nome: string;
+
+    @Column()
     kcal: number;
+
+    @Column()
     proteinas: number;
+    
+    @Column()
     carboidratos: number;
+    
+    @Column()
     godurasTotais: number;
 
 }

855655b79dc0b2db483bcd881c75465441bdb3fa
DanMarques31 <daniellmarques2003@hotmail.com>
2024-11-20 23:04:58 -0300

    Getting exercise.entity.ts into Type ORM

--- src/app/domain/exercise.entity.ts
+++ src/app/domain/exercise.entity.ts
@@ -1,7 +1,15 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('exercise')
 export class Exercise {
     
+    @PrimaryGeneratedColumn({ name: "exercise_id"} )
     id: number;
+    
+    @Column()
     nome: string;
+    
+    @Column()
     musculo: string;
 
 }
